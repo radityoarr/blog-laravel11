affcfdecde432c4455965e7292f4cb83657faf2a
Radityo <radityo890@gmail.com>
2024-08-15 15:02:07 +0700

    first commit

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Post;
+// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Category;
+use Illuminate\Support\Str;
+use Illuminate\Database\Seeder;
+use Database\Seeders\UserSeeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        // User::factory(10)->create();
+
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+        $this->call([CategorySeeder::class, UserSeeder::class]);
+
+        Post::factory(30)->recycle([
+            Category::all(),
+            User::all()
+        ])->create();
+
+    }
+}
---
+++ resources/views/about.blade.php
@@ -0,0 +1,5 @@
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot>
+    <h3 class="text-xl">About me</h3>
+    <p>Nama : {{ $name }}</p>
+</x-layout>
